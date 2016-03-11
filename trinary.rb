df20aeda241ac83b752c7317e9f33cc1b3d8cc32
Anton Malkov <anton.malkov@gmail.com>
2016-03-11 14:52:40 +0100

    Refactor into one-liner

--- trinary.rb
+++ trinary.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
 class Trinary
-  attr_accessor :trinary_str
+  attr_reader :trinary_str
 
   def initialize(trinary_str)
     @trinary_str = trinary_str
@@ -10,10 +8,6 @@ class Trinary
   def to_decimal
     return 0 if trinary_str =~ /[^0123]/
     digits = trinary_str.split('').map(&:to_i).reverse
-    decimal = 0
-    digits.each_with_index do |digit, index|
-      decimal += digit * (3 ** index)
-    end
-    decimal
+    digits.each_with_index.map { |d, i| d * (3 ** i) }.inject(:+)
   end
 end
